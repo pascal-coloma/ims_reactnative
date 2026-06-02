da47aa72bc646b2a5d42ecc532d24ce573d6b899
Pascal Coloma <coloma.pascal@gmail.com>
2026-06-02 13:25:59 -0400

    actualizacion de endpoints y formato de ambulancia

--- context/PacienteContext.tsx
+++ context/PacienteContext.tsx
@@ -24,7 +24,7 @@ const PacienteProvider = ({ children }: { children: ReactNode }) => {
       setLoading(true);
       setError(null);
       try {
-        const response = await fetchConSesion('/ims/api/pacientes/');
+        const response = await fetchConSesion('/ims/api/pacientes/get');
         if (!response.ok) throw new Error(`Error ${response.status}`);
         const data = await response.json();
         setPacientes(data);
