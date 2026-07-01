0dfe777f57ad54038ed1dedefa0a8ac039194d60
Pascal Coloma <coloma.pascal@gmail.com>
2026-07-01 00:56:27 -0400

    format

--- app/(user)/registrar-atencion.tsx
+++ app/(user)/registrar-atencion.tsx
@@ -98,7 +98,7 @@ const RegistrarAtencion = () => {
           },
           despachoActivo.ambulancia?.id ?? '',
         );
-        
+
         setFirmado(result?.success === 'Succeeded');
         reset();
         setExito(true);
